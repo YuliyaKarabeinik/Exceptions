ac4eabb84a4f538f436c0be98f17ea9d592e0c70
Yuliya Karabeinik <Yuliya.Karabeinik@thomsonreuters.com>
2020-03-08 13:42:37 +0300

    Add project files.

---
+++ Converter/StringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Converter
+{
+    public static class StringConverter
+    {
+        public static int? ParseString(string str)
+        {
+            try
+            {
+                return Convert.ToInt32(str);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Unable to convert:Exception: {e.Message}");
+                return null;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"Unable to convert:Exception: {e.Message}");
+                return null;
+            }
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,13 @@
+﻿using Converter;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "123";
+            var number = StringConverter.ParseString(str);
+        }
+    }
+}
